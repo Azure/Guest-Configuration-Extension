e60052ef43de3c757ce9d439df47f1f2ad082d66
Mehaa Gupta <mehaag@princeton.edu>
2018-08-14 01:18:51 -0700

    added some more unit tests and created some more telemetry events

--- main/cmds_test.go
+++ main/cmds_test.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/go-kit/kit/log"
 	"github.com/stretchr/testify/require"
+	"github.com/Azure/azure-docker-extension/pkg/vmextension"
 )
 
 func Test_commandsExist(t *testing.T) {
@@ -35,14 +36,14 @@ func Test_commands_shouldReportStatus(t *testing.T) {
 	require.True(t, cmds["update"].shouldReportStatus, "update should report status")
 }
 
-func Test_checkAndSaveSeqNum_fails(t *testing.T) {
+func Test_checkAndSaveSeqNum_fail(t *testing.T) {
 	// pass in invalid seqnum format
 	_, err := checkAndSaveSeqNum(log.NewNopLogger(), 0, "/non/existing/dir")
 	require.NotNil(t, err)
 	require.Contains(t, err.Error(), `failed to save the sequence number`)
 }
 
-func Test_checkAndSaveSeqNum(t *testing.T) {
+func Test_checkAndSaveSeqNum_success(t *testing.T) {
 	dir, err := ioutil.TempDir("", "")
 	require.Nil(t, err)
 	fp := filepath.Join(dir, "seqnum")
@@ -86,6 +87,14 @@ func Test_parseVersionString_success(t *testing.T) {
 	require.Nil(t, err)
 }
 
+func Test_runCmd_fail(t *testing.T) {
+	dir, err := ioutil.TempDir("", "")
+	require.Nil(t, err)
+	defer os.RemoveAll(dir)
+
+	require.NotNil(t, runCmd(log.NewNopLogger(), "wrongCmd", dir, handlerSettings{}))
+}
+
 func Test_runCmd_success(t *testing.T) {
 	dir, err := ioutil.TempDir("", "")
 	require.Nil(t, err)
@@ -100,15 +109,14 @@ func Test_runCmd_success(t *testing.T) {
 	require.Nil(t, err, "stdout should exist")
 	_, err = os.Stat(filepath.Join(dir, "stderr"))
 	require.Nil(t, err, "stderr should exist")
-}
 
-func Test_runCmd_pass(t *testing.T) {
-	dir, err := ioutil.TempDir("", "")
-	require.Nil(t, err)
-	defer os.RemoveAll(dir)
+	require.Nil(t, runCmd(log.NewNopLogger(), "", dir, handlerSettings{}))
 
-	err = runCmd(log.NewNopLogger(), "", dir, handlerSettings{})
-	require.Nil(t, err)
+	// check stdout stderr files
+	_, err = os.Stat(filepath.Join(dir, "stdout"))
+	require.Nil(t, err, "stdout should exist")
+	_, err = os.Stat(filepath.Join(dir, "stderr"))
+	require.Nil(t, err, "stderr should exist")
 }
 
 func Test_runCmd_withTestFile(t *testing.T) {
@@ -137,23 +145,37 @@ func Test_unzip_fail(t *testing.T) {
 }
 
 func Test_unzip_pass(t *testing.T) {
-	version := "0.0.1"
-	dir := filepath.Join(dataDir, agentDir, version)
+	dir := filepath.Join(dataDir, agentDir)
 	filenames, err := unzip(log.NewNopLogger(), "../"+agentZip, dir)
 	require.Nil(t, err)
 	require.NotEmpty(t, filenames)
 
-	version = "1.2.0"
-	dir = filepath.Join(dataDir, agentDir, version)
+	dir = filepath.Join(dataDir, agentDir)
 	filenames, err = unzip(log.NewNopLogger(), "../"+agentZip, dir)
 	require.Nil(t, err)
 	require.NotEmpty(t, filenames)
 
-	version = "3.0.0"
-	dir = filepath.Join(dataDir, agentDir, version)
-	filenames, err = unzip(log.NewNopLogger(), "../"+agentZip, dir)
+	Test_cleanUpTests(t)
+}
+
+func Test_install(t *testing.T) {
+	message, err := install(log.NewNopLogger(), vmextension.HandlerEnvironment{}, 0)
 	require.Nil(t, err)
-	require.NotEmpty(t, filenames)
+	require.Empty(t, message)
+}
+
+
+func Test_enablePre(t *testing.T) {
+	dir := filepath.Join(mostRecentSequence)
+	os.RemoveAll(dir)
+	nop := log.NewNopLogger()
+	defer os.RemoveAll(dir)
+
+	err := enablePre(nop, 0)
+	require.Nil(t, err)
+
+	err = enablePre(nop, 1)
+	require.Nil(t, err)
 }
 
 func Test_cleanUpTests(t *testing.T) {
