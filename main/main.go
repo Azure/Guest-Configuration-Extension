9f9a32c50e1a10a78e157ced03d3590b9161dd83
Amit Saraf <amitsara@microsoft.com>
2022-03-03 17:37:07 -0800

    check for unsupported distro at shim layer and exit if not supported

--- main/main.go
+++ main/main.go
@@ -71,20 +71,14 @@ func main() {
 	lg.event("Reporting transitioning status...")
 	reportStatus(lg, hEnv, seqNum, status.StatusTransitioning, cmd, "Transitioning")
 
-	if err_code, cmdErr := cmd.f(lg, hEnv, seqNum); cmdErr != nil {
+	if cmdErr := cmd.f(lg, hEnv, seqNum); cmdErr != nil {
 		message := "Operation '" + cmd.name + "' failed."
 		lg.eventError(message, cmdErr)
 		telemetry(TelemetryScenario, message+" Error: '"+cmdErr.Error()+"'.", false, 0)
 		// Never fail on disable due to a current bug in the Guest Agent
 		if cmd.name != "disable" {
-			if err_code == 51 {
-				telemetry(TelemetryScenario, "Exiting with error code : 51", false, 0)
-				reportStatus(lg, hEnv, seqNum, status.StatusError, cmd, "UnsupportedOS")
-				os.Exit(err_code)
-			} else {
-				reportStatus(lg, hEnv, seqNum, status.StatusError, cmd, cmdErr.Error())
-				os.Exit(cmd.failExitCode)
-			}
+			reportStatus(lg, hEnv, seqNum, status.StatusError, cmd, cmdErr.Error())
+			os.Exit(cmd.failExitCode)
 		}
 	} else {
 		message := "Operation '" + cmd.name + "' succeeded."
